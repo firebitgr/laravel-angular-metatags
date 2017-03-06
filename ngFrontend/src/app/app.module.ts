a0ce7418cce505420fb15444953088a127d13a8a
blAck PR <blackpr.tim@gmail.com>
2017-03-06 15:14:33 +0200

    add routing

--- ngFrontend/src/app/app.module.ts
+++ ngFrontend/src/app/app.module.ts
@@ -2,10 +2,16 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
+import { RouterModule, Routes } from '@angular/router';
 
 import { AppComponent } from './app.component';
 import { FirstComponent } from './first/first.component';
 import { SecondComponent } from './second/second.component';
+
+const appRoutes: Routes = [
+  { path: 'first', component: FirstComponent },
+  { path: 'second/:id', component: SecondComponent },
+];
 
 @NgModule({
   declarations: [
@@ -16,7 +22,8 @@ import { SecondComponent } from './second/second.component';
   imports: [
     BrowserModule,
     FormsModule,
-    HttpModule
+    HttpModule,
+    RouterModule.forRoot(appRoutes)
   ],
   providers: [],
   bootstrap: [AppComponent]
